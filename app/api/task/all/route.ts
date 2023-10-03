01289a17916efbcd397aa288650ea714bd9655da
pierre merlaud <pmerlaud007@gmail.com>
2023-10-03 12:57:28 +0200

    set cache control in api/task/all

--- app/api/task/all/route.ts
+++ app/api/task/all/route.ts
@@ -9,6 +9,12 @@ export const GET = async (request: Request) => {
 
     const tasks = await Task.find({});
 
+    // Set Cache-Control header to disable caching
+    request.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, max-age=0, must-revalidate"
+    );
+
     return NextResponse.json(tasks, { status: 200 });
   } catch (error) {
     console.log(error);
